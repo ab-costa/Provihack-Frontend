00db4f63547065e5be9b9f8a8ba939dd31e74701
ab-costa <costa.alessandroborges@gmail.com>
2021-11-27 22:03:56 -0300

    commit incial

---
+++ src/components/Input.js
@@ -0,0 +1,12 @@
+// function Input({ test }) {
+//     const [test, setTest] = useState('');
+
+//     return(
+//         <>
+//             {test}
+//             <input type="text" onChange={(e) => setTest(e.target.value)} value={test} />
+//         </>
+//     );
+// }
+
+// export default Input;
